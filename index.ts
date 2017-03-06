9d4c9d426b925092adfa2b5d5ca6519572280e59
Greg L Nelson <glnelson@uw.edu>
2017-03-05 16:38:43 -0800

    rankings compiles; next check it works

--- index.ts
+++ index.ts
@@ -126,8 +126,8 @@ function buildProgram(fields: {name: string, type: string, cardinality: number}[
   // FIXME: greg
   const [defs, minimizeStmt] = ranking(fields, query, encs)
   console.log(defs);
-  //program += defs;
-  //program += minimizeStmt;
+  program += defs;
+  program += minimizeStmt;
 
   program += solve;
 
